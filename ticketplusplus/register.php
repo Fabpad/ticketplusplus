52787457a6e4e5e2e78cce8193b6a068441f76c3
Fabpad <38310323+Fabpad@users.noreply.github.com>
2018-04-12 13:26:17 +0200

    New design
    added bootstrap
    changed layout from default to temp design

--- ticketplusplus/register.php
+++ ticketplusplus/register.php
@@ -9,51 +9,62 @@ include_once 'includes/functions.php';
         <title>Secure Login: Registration Form</title>
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script>
-        <link rel="stylesheet" href="styles/main.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     </head>
     <body>
-        <!-- Anmeldeformular für die Ausgabe, wenn die POST-Variablen nicht gesetzt sind
-        oder wenn das Anmelde-Skript einen Fehler verursacht hat. -->
-        <h1>Register with us</h1>
+	
         <?php
         if (!empty($error_msg)) {
             echo $error_msg;
         }
         ?>
-        <ul>
-            <li>Benutzernamen dürfen nur Ziffern, Groß- und Kleinbuchstaben und Unterstriche enthalten.</li>
-            <li>E-Mail-Adressen müssen ein gültiges Format haben.</li>
-            <li>Passwörter müssen mindest sechs Zeichen lang sein.</li>
-            <li>Passwörter müssen enthalten
-                <ul>
-                    <li>mindestens einen Großbuchstaben (A..Z)</li>
-                    <li>mindestens einen Kleinbuchstabenr (a..z)</li>
-                    <li>mindestens eine Ziffer (0..9)</li>
-                </ul>
-            </li>
-            <li>Das Passwort und die Bestätigung müssen exakt übereinstimmen.</li>
-        </ul>
-        <form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" 
-                method="post" 
-                name="registration_form">
-            Username: <input type='text' 
-                name='username' 
-                id='username' /><br>
-            Email: <input type="text" name="email" id="email" /><br>
-            Password: <input type="password"
-                             name="password" 
-                             id="password"/><br>
-            Confirm password: <input type="password" 
-                                     name="confirmpwd" 
-                                     id="confirmpwd" /><br>
-            <input type="button" 
-                   value="Register" 
-                   onclick="return regformhash(this.form,
-                                   this.form.username,
-                                   this.form.email,
-                                   this.form.password,
-                                   this.form.confirmpwd);" /> 
-        </form>
-        <p>Return to the <a href="login.php">login page</a>.</p>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-md-12 col-lg-12">
+					<h1>Nutzer anlegen</h1>
+				</div>
+				<div class="col-sm-12 col-md-4 col-lg-4">
+					<form action="<?php echo esc_url($_SERVER['PHP_SELF']); ?>" method="post" name="registration_form">
+			
+						<input type="text" class="form-control" name='username' id='username' placeholder="Nutzername" /><br>
+
+						<input type="text" class="form-control" name="email" id="email" placeholder="Email" /><br>
+						
+						<input type="password" class="form-control" name="password" id="password" placeholder="Passwort" /><br>
+						
+						<input type="password" class="form-control" name="confirmpwd" id="confirmpwd" placeholder="Passwort bestätigen" /><br>
+						
+						<input type="button" class="btn btn-secondary" value="Registrieren" onclick="return regformhash(this.form,this.form.username,this.form.email,this.form.password,this.form.confirmpwd);" /> 
+						
+					</form>
+				</div>
+				
+				<div class="col-sm-12 col-md-8 col-lg-8">
+					<ul class="list-group">
+						<li class="list-group-item">Benutzernamen dürfen nur Ziffern, Groß- und Kleinbuchstaben und Unterstriche enthalten.</li>
+						<li class="list-group-item">E-Mail-Adressen müssen ein gültiges Format haben.</li>
+						<li class="list-group-item">Passwörter müssen mindestens sechs Zeichen lang sein.</li>
+						<li class="list-group-item">Passwörter müssen enthalten
+							<ul>
+								<li >mindestens einen Großbuchstaben (A..Z)</li>
+								<li >mindestens einen Kleinbuchstaben (a..z)</li>
+								<li >mindestens eine Ziffer (0..9)</li>
+							</ul>
+						</li>
+						<li class="list-group-item">Das Passwort und die Bestätigung müssen exakt übereinstimmen.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<footer class="page-footer pt-4 mt-4">
+			<div class="container-fluid text-center text-md-left">
+				<div class="row">
+					<div class="col-md-12">
+						<p>Zurück zur <a href="login.php">Anmeldeseite</a>.</p>
+					</div>
+				</div>
+			</div>
+		</footer>
     </body>
 </html>
