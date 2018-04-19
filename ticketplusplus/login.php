e692f502d2025bea484bcf738d17197988b0c6af
Fabpad <38310323+Fabpad@users.noreply.github.com>
2018-04-19 09:57:04 +0200

    Revert "Merge branch 'master' of https://github.com/Fabpad/ticketplusplus"
    This reverts commit 11f2e793cea29c7a7b33c0e2c186f87abee84cc0, reversing
    changes made to 6177d396160dfc2e33a4d2ae3b9d41e0763ad445.

--- ticketplusplus/login.php
+++ ticketplusplus/login.php
@@ -13,25 +13,27 @@ if (login_check($mysqli) == true) {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ticket++</title>
+	<title>Secure Login: Log In</title>
 	<link rel="stylesheet" href="styles/main.css" />
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<script type="text/JavaScript" src="js/sha512.js"></script> 
 	<script type="text/JavaScript" src="js/forms.js"></script> 
 </head>
-<body style="background-color:#FE9A2E;">
+<body style="background-color:#ffb516">
 	<?php
         if (isset($_GET['error'])) {
             echo '<p class="error">Error Logging In!</p>';
         }
     ?> 
-       <form action="includes/process_login.php" method="post" name="login_form">                      
+		<div class="imgbox">
+			<img class="center-fit" src="assets/TPPlogo2.png" alt="Logo" style="width:auto;">
+		</div>
+		<form action="includes/process_login.php" method="post" name="login_form">                      
 		Email: </br>
-		<input type="text" size="40" maxlength="250" name="email" id="email" /></br>
+		<input type="text" size="40" maxlength="250" name="email" /></br>
 		Password: </br>
 		<input type="password" size="40" maxlength="250" name="password" id="password"/></br></br>
-		<input type="button" class="btn btn-secondary" value="Login" onclick="formhash(this.form, this.form.password);" id="login" />
-		<script type="text/JavaScript" src="js/login_on_enter.js"></script>
+		<input type="button" class="btn btn-secondary" value="Login" onclick="formhash(this.form, this.form.password);" />
 		<p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
 		<p>You are currently logged <?php echo $logged ?>.</p>				   
        </form>
