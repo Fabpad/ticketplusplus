1a7a8aa8606cb6b9a0925eac1c3f8f06e78e5ef6
qeqqex <38314609+qeqqex@users.noreply.github.com>
2018-04-25 13:49:41 +0200

    jquery

--- ticketplusplus/ticketview.php
+++ ticketplusplus/ticketview.php
@@ -12,7 +12,7 @@ sec_session_start();
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 	<script type="text/JavaScript" src="js/fill_specifications.js"></script> 
-	<script type ="text/JavaScript" src="js/jquery-3.3.1.js"></script>
+	<script type ="text/JavaScript" src="js/jQuery-3.3.1.js"></script>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">	
 	<link rel="stylesheet" href="styles/ticketview.css" />	
