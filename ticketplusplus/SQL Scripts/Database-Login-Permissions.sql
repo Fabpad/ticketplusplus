91f9f406f328f9a0cc0d3d9ae7bd55840d388840
Fabpad <38310323+Fabpad@users.noreply.github.com>
2018-04-25 10:28:37 +0200

    Fixed Errors

--- ticketplusplus/SQL Scripts/Database-Login-Permissions.sql
+++ ticketplusplus/SQL Scripts/Database-Login-Permissions.sql
@@ -1,37 +1,37 @@
-CREATE DATABASE 'ticketplusplus';
+CREATE DATABASE ticketplusplus;
 
-CREATE TABLE 'ticketplusplus'.'users' (
-    'id' INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    'username' VARCHAR(30) NOT NULL,
-    'email' VARCHAR(50) NOT NULL,
-    'password' CHAR(128) NOT NULL,
-    'salt' CHAR(128) NOT NULL 
+CREATE TABLE ticketplusplus.users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password CHAR(128) NOT NULL,
+    salt CHAR(128) NOT NULL 
 ) ENGINE = InnoDB;
 
-CREATE TABLE 'ticketplusplus'.'login_attempts' (
-    'user_id' INT(11) NOT NULL,
-    'time' VARCHAR(30) NOT NULL
-) ENGINE=InnoDB
+CREATE TABLE ticketplusplus.login_attempts (
+    user_id INT(11) NOT NULL,
+    time VARCHAR(30) NOT NULL
+) ENGINE=InnoDB;
 
-INSERT INTO 'ticketplusplus'.'users' VALUES(1, 'test_user', 'test@example.com',
+INSERT INTO ticketplusplus.users VALUES(1, 'test_user', 'test@example.com',
 '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc',
 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef');
 
-CREATE TABLE 'ticketplusplus'.'roles' (
+CREATE TABLE ticketplusplus.roles (
   role_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   role_name VARCHAR(50) NOT NULL,
 
   PRIMARY KEY (role_id)
 );
 
-CREATE TABLE 'ticketplusplus'.'permissions' (
+CREATE TABLE ticketplusplus.permissions (
   perm_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   perm_desc VARCHAR(50) NOT NULL,
 
   PRIMARY KEY (perm_id)
 );
 
-CREATE TABLE 'ticketplusplus'.'role_perm' (
+CREATE TABLE ticketplusplus.role_perm (
   role_id INTEGER UNSIGNED NOT NULL,
   perm_id INTEGER UNSIGNED NOT NULL,
 
@@ -39,7 +39,7 @@ CREATE TABLE 'ticketplusplus'.'role_perm' (
   FOREIGN KEY (perm_id) REFERENCES permissions(perm_id)
 );
 
-CREATE TABLE 'ticketplusplus'.'user_role' (
+CREATE TABLE ticketplusplus.user_role (
   user_id INTEGER UNSIGNED NOT NULL,
   role_id INTEGER UNSIGNED NOT NULL,
 
