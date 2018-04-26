9124a1f2857a500d2afc6f1a444641cc44464562
qeqqex <38314609+qeqqex@users.noreply.github.com>
2018-04-26 13:51:59 +0200

    Database

---
+++ ticketplusplus/SQL Scripts/neusteVersion.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 26. Apr 2018 um 13:51
+-- Server-Version: 10.1.28-MariaDB
+-- PHP-Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `ticketplusplus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(10) UNSIGNED NOT NULL,
+  `beschreibung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `category`
+--
+
+INSERT INTO `category` (`category_id`, `beschreibung`) VALUES
+(1, 'Hardware'),
+(2, 'Software'),
+(3, 'Organisation');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `department`
+--
+
+CREATE TABLE `department` (
+  `dept_id` int(10) UNSIGNED NOT NULL,
+  `beschreibung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `department`
+--
+
+INSERT INTO `department` (`dept_id`, `beschreibung`) VALUES
+(1, 'Marketing'),
+(2, 'Einkauf'),
+(3, 'IT'),
+(4, 'Produktion'),
+(5, 'Buchhaltung');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `login_attempts`
+--
+
+CREATE TABLE `login_attempts` (
+  `user_id` int(11) NOT NULL,
+  `time` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `login_attempts`
+--
+
+INSERT INTO `login_attempts` (`user_id`, `time`) VALUES
+(2, '1524722235'),
+(2, '1524726978');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `priority`
+--
+
+CREATE TABLE `priority` (
+  `priotity_id` int(10) UNSIGNED NOT NULL,
+  `beschreibung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `priority`
+--
+
+INSERT INTO `priority` (`priotity_id`, `beschreibung`) VALUES
+(1, 'High'),
+(2, 'Normal'),
+(3, 'Low');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `roles`
+--
+
+CREATE TABLE `roles` (
+  `role_id` int(10) UNSIGNED NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `roles`
+--
+
+INSERT INTO `roles` (`role_id`, `role_name`) VALUES
+(1, 'User'),
+(2, 'Agent'),
+(3, 'Admin');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `specification`
+--
+
+CREATE TABLE `specification` (
+  `specification_id` int(10) UNSIGNED NOT NULL,
+  `beschreibung` varchar(30) NOT NULL,
+  `category_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `specification`
+--
+
+INSERT INTO `specification` (`specification_id`, `beschreibung`, `category_id`) VALUES
+(1, 'Anforderung neue Hardware', 1),
+(2, 'Drucker defekt', 1),
+(3, 'PC defekt', 1),
+(4, 'WLAN / LAN defekt', 1),
+(5, 'Sonstiges', 1),
+(6, 'OS', 2),
+(7, 'Programmanforderung', 2),
+(8, 'Programm defekt', 2),
+(9, 'Sonstiges', 2),
+(10, 'Benutzer anlegen', 3),
+(11, 'Benutzer entsperren', 3),
+(12, 'Kennwort vergessen', 3),
+(13, 'Speicherplatz erweitern', 3),
+(14, 'Sonstiges', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(10) UNSIGNED NOT NULL,
+  `beschreibung` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `status`
+--
+
+INSERT INTO `status` (`status_id`, `beschreibung`) VALUES
+(1, 'Abgeschlossen'),
+(2, 'In Bearbeitung'),
+(3, 'Warten'),
+(4, 'Offen');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` char(128) NOT NULL,
+  `salt` char(128) NOT NULL,
+  `vorname` varchar(40) NOT NULL,
+  `nachname` varchar(40) NOT NULL,
+  `telefonnummer` varchar(40) NOT NULL,
+  `dept_id` int(10) UNSIGNED NOT NULL,
+  `role_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `salt`, `vorname`, `nachname`, `telefonnummer`, `dept_id`, `role_id`) VALUES
+(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef', 'Test', 'tesT', '1293135628', 1, 1),
+(2, 'dpanhoff', 'daniel@test.de', '2017e1fa551e6caa41434eab4a5662c5ac42c2341a32ada9cf1afb16b859e920bdd39e0cc0b32228eb50ab3796e2d3d8ace6086015bb47cdbe74c74ac029fede', '1072c7f2670ec72c6c8d0ebff4490e52c0b38bf8a1ab0da73d65b7942ab728ca234ba67025e42280c812801042c357fa93aefa6475bde515bf0dcd0beef222b1', 'Daniel', 'Panhoff', '01234567890', 2, 2),
+(4, 'User', 'user1@asd.de', '8d04b842686803b83e8e857d02fe2398ea0028c4c590e2b6ad343a690cba1907381dc05f501982494c109c16ba016155450a631f1bd08ce23fe911cdb10abc34', 'edc991b244ff5035a6ef8b37aa6e919487a38e9dd9c0acc5e1301ed984a8f394aaf54b0fdc97a1d09fb08c2f51201f4de1ce0fa2a384c6de89be8f7230fc0672', 'User', 'useR', '123234534645', 4, 1);
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indizes für die Tabelle `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`dept_id`);
+
+--
+-- Indizes für die Tabelle `priority`
+--
+ALTER TABLE `priority`
+  ADD PRIMARY KEY (`priotity_id`);
+
+--
+-- Indizes für die Tabelle `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indizes für die Tabelle `specification`
+--
+ALTER TABLE `specification`
+  ADD PRIMARY KEY (`specification_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indizes für die Tabelle `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`),
+  ADD KEY `dept_id` (`dept_id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `department`
+--
+ALTER TABLE `department`
+  MODIFY `dept_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT für Tabelle `priority`
+--
+ALTER TABLE `priority`
+  MODIFY `priotity_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `specification`
+--
+ALTER TABLE `specification`
+  MODIFY `specification_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT für Tabelle `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `specification`
+--
+ALTER TABLE `specification`
+  ADD CONSTRAINT `specification_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`);
+
+--
+-- Constraints der Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`dept_id`) REFERENCES `department` (`dept_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
