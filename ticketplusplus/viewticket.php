6f44e6e4a199e9a4d611cb425d362ce09aec8ee6
NathalieT98 <38314266+NathalieT98@users.noreply.github.com>
2018-05-03 09:45:43 +0200

    view ticket
    ausgrauen Felder

--- ticketplusplus/viewticket.php
+++ ticketplusplus/viewticket.php
@@ -8,18 +8,18 @@
 	
 	<div class="ml-5 mt-5 col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
 		<label for="txt_betreff">Betreff</label>
-		<input class="form-control" id="txt_betreff" type="text" placeholder="Benutzer entsperren, Speicherplatz erweitern, PC installieren ..." aria-label="Betreff" />
+		<input class="form-control" id="txt_betreff" type="text" placeholder="Benutzer entsperren, Speicherplatz erweitern, PC installieren ..." aria-label="Betreff" readonly="readonly" />
 	</div>
 	
 	<div class="ml-5 mt-3 mr-5 col-11 col-sm-11 col-md-11 col-lg-11 col-xl-11">
 		<label for="txt_beschreibung">Beschreibung</label>
-		<textarea class="form-control" id="txt_beschreibung" placeholder="Beschreibung" aria-label="Beschreibung" rows="20" style="resize:none"></textarea>
+		<textarea class="form-control" id="txt_beschreibung" placeholder="Beschreibung" aria-label="Beschreibung" rows="20" style="resize:none" readonly="readonly"></textarea>
 	</div>
 	
 	<div class="ml-5 mt-3 col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 row">
 		<div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
 			<label for="txt_user">Benutzer</label>
-			<input class="form-control" type="text" id="txt_user">
+			<input class="form-control" type="text" id="txt_user" readonly="readonly">
 		</div>
 		<div class="ml-2 col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
 			<label for="status_menu">Status</label>
@@ -60,8 +60,8 @@
 	<div class="ml-5 mt-3 col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 row">
 		<div class="col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
 			<label for="category_menu">Kategorie</label>
-			<select class="custom-select d-block w-100" id="category_menu" required>
-				<option value=""> --- Bitte wählen --- </option>
+			<select disabled class="custom-select d-block w-100" id="category_menu" required>
+				<option < value=""> --- Bitte wählen --- </option>
 				<?php
 					//Run Query
 					$stmt = "SELECT DISTINCT beschreibung FROM ticketplusplus.category";
@@ -77,8 +77,8 @@
 		</div>
 		<div class="ml-2 col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
 			<label for="specification_menu">Unterkategorie</label>
-			<select class="custom-select d-block w-100" id="specification_menu" required>
-				<option value=""> --- Eine Kategorie wählen --- </option>
+			<select disabled class="custom-select d-block w-100" id="specification_menu" required >
+				<option disabled="disabled" value=""> --- Eine Kategorie wählen --- </option>
 			</select>
 			<div class="invalid-feedback">
 				Bitte eine Unterkategorie auswählen.
