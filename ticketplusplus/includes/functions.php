754d2bab1237eeed4978148d8077c2cd51c63c7f
Fabpad <38310323+Fabpad@users.noreply.github.com>
2018-05-11 16:49:15 +0200

    Changed Ticketoverview
    Users only see their own tickets
    
    Agents see all the tickets which they are in charge of
    
    Admins see all the tickets

--- ticketplusplus/includes/functions.php
+++ ticketplusplus/includes/functions.php
@@ -194,13 +194,20 @@ function fillTicketTable($username, $mysqli) {
 	$result = mysqli_query($mysqli,$stmt) or die(mysqli_error($mysqli));
 	while(list($temp) = mysqli_fetch_row($result)){
 		$userid = $temp;
-	}
+    }
 
-	$stmt = "SELECT tickets.ticket_id, tickets.betreff, status.beschreibung AS Stat, users.username, priority.beschreibung AS Prio, tickets.erstell_datum 
-				FROM ticketplusplus.tickets, ticketplusplus.status, ticketplusplus.users, ticketplusplus.priority
-				WHERE users.id = '$userid' AND status.status_id = tickets.status_id AND priority.priority_id = tickets.priority_id AND tickets.agent_id = users.id"; 
+    $stmt = "SELECT role_id FROM ticketplusplus.users WHERE username = '$username'";
 	$result = mysqli_query($mysqli,$stmt) or die(mysqli_error($mysqli));
-	while($ticket = mysqli_fetch_row($result)) {
+	while(list($temp) = mysqli_fetch_row($result)){
+		$role = $temp;
+    }
+
+    if ($role == 1) {
+        $stmt = "SELECT tickets.ticket_id, tickets.betreff, status.beschreibung AS Stat, users.username, priority.beschreibung AS Prio, tickets.erstell_datum 
+				FROM ticketplusplus.tickets, ticketplusplus.status, ticketplusplus.users, ticketplusplus.priority
+				WHERE users.id = '$userid' AND status.status_id = tickets.status_id AND priority.priority_id = tickets.priority_id AND tickets.user_id = '$userid'"; 
+	    $result = mysqli_query($mysqli,$stmt) or die(mysqli_error($mysqli));
+	    while($ticket = mysqli_fetch_row($result)) {
 			$ticketid = $ticket[0];
 			$ticketbetreff = $ticket[1];
 			$ticketstat = $ticket[2];
@@ -209,5 +216,38 @@ function fillTicketTable($username, $mysqli) {
 			$ticketdate = $ticket[5];
 			
 			echo "<tr><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketid."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketbetreff."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketstat."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketma."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketprio."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketdate."</a></td></tr>";
-	}	
+	    }	
+    }
+    else if ($role == 2) {
+        $stmt = "SELECT tickets.ticket_id, tickets.betreff, status.beschreibung AS Stat, users.username, priority.beschreibung AS Prio, tickets.erstell_datum 
+				FROM ticketplusplus.tickets, ticketplusplus.status, ticketplusplus.users, ticketplusplus.priority
+				WHERE users.id = '$userid' AND status.status_id = tickets.status_id AND priority.priority_id = tickets.priority_id AND tickets.agent_id = users.id"; 
+	    $result = mysqli_query($mysqli,$stmt) or die(mysqli_error($mysqli));
+	    while($ticket = mysqli_fetch_row($result)) {
+			$ticketid = $ticket[0];
+			$ticketbetreff = $ticket[1];
+			$ticketstat = $ticket[2];
+			$ticketma = $ticket[3];
+			$ticketprio = $ticket[4];
+			$ticketdate = $ticket[5];
+			
+			echo "<tr><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketid."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketbetreff."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketstat."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketma."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketprio."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketdate."</a></td></tr>";
+	    }	
+    }
+    else if ($role == 3) {
+        $stmt = "SELECT tickets.ticket_id, tickets.betreff, status.beschreibung AS Stat, users.username, priority.beschreibung AS Prio, tickets.erstell_datum 
+				FROM ticketplusplus.tickets, ticketplusplus.status, ticketplusplus.users, ticketplusplus.priority
+				WHERE users.id = '$userid' AND status.status_id = tickets.status_id AND priority.priority_id = tickets.priority_id";
+	    $result = mysqli_query($mysqli,$stmt) or die(mysqli_error($mysqli));
+	    while($ticket = mysqli_fetch_row($result)) {
+			$ticketid = $ticket[0];
+			$ticketbetreff = $ticket[1];
+			$ticketstat = $ticket[2];
+			$ticketma = $ticket[3];
+			$ticketprio = $ticket[4];
+			$ticketdate = $ticket[5];
+			
+			echo "<tr><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketid."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketbetreff."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketstat."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketma."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketprio."</a></td><td><a href='viewticket.php?ticketid=$ticketid'>".$ticketdate."</a></td></tr>";
+	    }	
+    }
 }
