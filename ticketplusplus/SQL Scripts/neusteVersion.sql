6f49ad633d09db5cbba4e8b8227aa78911ccd88e
Fabpad <38310323+Fabpad@users.noreply.github.com>
2018-05-14 13:10:53 +0200

    sql script now creates an superadmin
    pw Test1234

--- ticketplusplus/SQL Scripts/neusteVersion.sql
+++ ticketplusplus/SQL Scripts/neusteVersion.sql
@@ -200,9 +200,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `salt`, `vorname`, `nachname`, `telefonnummer`, `dept_id`, `role_id`) VALUES
-(1, 'test_user', 'test@example.com', '00807432eae173f652f2064bdca1b61b290b52d40e429a7d295d76a71084aa96c0233b82f1feac45529e0726559645acaed6f3ae58a286b9f075916ebf66cacc', 'f9aab579fc1b41ed0c44fe4ecdbfcdb4cb99b9023abb241a6db833288f4eea3c02f76e0d35204a8695077dcf81932aa59006423976224be0390395bae152d4ef', 'Test', 'tesT', '1293135628', 1, 1),
-(2, 'dpanhoff', 'daniel@test.de', '2017e1fa551e6caa41434eab4a5662c5ac42c2341a32ada9cf1afb16b859e920bdd39e0cc0b32228eb50ab3796e2d3d8ace6086015bb47cdbe74c74ac029fede', '1072c7f2670ec72c6c8d0ebff4490e52c0b38bf8a1ab0da73d65b7942ab728ca234ba67025e42280c812801042c357fa93aefa6475bde515bf0dcd0beef222b1', 'Daniel', 'Panhoff', '01234567890', 2, 2),
-(4, 'User', 'user1@asd.de', '8d04b842686803b83e8e857d02fe2398ea0028c4c590e2b6ad343a690cba1907381dc05f501982494c109c16ba016155450a631f1bd08ce23fe911cdb10abc34', 'edc991b244ff5035a6ef8b37aa6e919487a38e9dd9c0acc5e1301ed984a8f394aaf54b0fdc97a1d09fb08c2f51201f4de1ce0fa2a384c6de89be8f7230fc0672', 'User', 'useR', '123234534645', 4, 1);
+(1, 'superadmin', 'superadmin@tpp.de', '2017e1fa551e6caa41434eab4a5662c5ac42c2341a32ada9cf1afb16b859e920bdd39e0cc0b32228eb50ab3796e2d3d8ace6086015bb47cdbe74c74ac029fede', '1072c7f2670ec72c6c8d0ebff4490e52c0b38bf8a1ab0da73d65b7942ab728ca234ba67025e42280c812801042c357fa93aefa6475bde515bf0dcd0beef222b1', 'Super', 'Admin', '01234567890', 2, 3);
 
 --
 -- Indizes der exportierten Tabellen
